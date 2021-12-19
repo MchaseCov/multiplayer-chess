ba21653ea7d70cbfce8ca28a8f867569ca0287aa
Chase Covington <mchasecov@gmail.com>
2021-12-19 12:36:00 -0500

    docs: User schema comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,25 @@
+# == Table Schema ==
+#
+# table name: scrollers
+#
+# id                      :bigint       null: false, primary key
+# username                :string
+# encrypted_password      :string       null: false
+# created_at              :datetime     null: false
+# updated_at              :datetime     null: false
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Callbacks
+  # Scopes
+  # Validations
+  validates_presence_of :username
+  validates_uniqueness_of :username
+  # Associations
+  # Methods
   def email_required?
     false
   end
