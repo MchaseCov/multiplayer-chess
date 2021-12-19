c9417ed099bb308eae6d2e1468a66e3c21e2a175
Chase Covington <mchasecov@gmail.com>
2021-12-19 15:20:28 -0500

    feat: create and show games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,13 @@
 class GamesController < ApplicationController
-  def index; end
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.where(id: params[:id]).includes({ squares: :piece }, :pieces).first
+  end
+
+  def create
+    current_user.white_games.create
+  end
 end
